9c9fe9e034716f25fdc2e9c747ae0b90cb06845e
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2026-05-04 15:06:37 +0200

    chore: add Serilog configuration

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -1,4 +1,17 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+    .WriteTo.File("Logs/api-.log",
+        rollingInterval: RollingInterval.Day,
+        outputTemplate: "{Timestamp:dd-MM-yyyy HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 builder.Services.AddOpenApi();
 
@@ -10,6 +23,20 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+try
+{
+    Log.Information("application start");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "application terminated with fatal ex");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
 
 app.Run();
 
